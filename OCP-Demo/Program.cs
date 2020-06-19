eb87a971479850f903a472c657e11b4b8e6e782d
Mdsomratakbor <somrat876@gmail.com>
2020-06-19 22:16:31 +0600

    Some change every class

--- OCP-Demo/Program.cs
+++ OCP-Demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OCPLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,25 @@ namespace OCP_Demo
     {
         static void Main(string[] args)
         {
+            List<PersonModel> applicants = new List<PersonModel> {
+             new PersonModel { FirstName = "Moazzam", LastName = "Hossain" },
+             new PersonModel { FirstName = "Md Somrat", LastName = "Akbor" },
+             new PersonModel { FirstName = "Md Raihan", LastName = "Bhuian" },
+             new PersonModel { FirstName = "Md Delwar", LastName = "Bhuian" }
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accountProcessor = new Accounts();
+            foreach (var person in applicants)
+            {
+                employees.Add(accountProcessor.Create(person));
+            }
+            foreach(var employee in employees)
+            {
+                Console.WriteLine($"FirstName :{employee.FirstName}, LastName :{employee.LastName}, EmailAddress :{employee.EmailAddress} {Environment.NewLine}");
+            }
+            Console.ReadLine();
+
         }
     }
 }
--- OCPLibrary/Accounts.cs
+++ OCPLibrary/Accounts.cs
@@ -8,5 +8,13 @@ namespace OCPLibrary
 {
     public class Accounts
     {
+       public EmployeeModel Create(PersonModel person)
+        {
+            EmployeeModel output = new EmployeeModel();
+            output.FirstName = person.FirstName;
+            output.LastName = person.LastName;
+            output.EmailAddress = $"{person.FirstName.Substring(0, 1)}{person.LastName}@gamil.com";
+            return output;
+        }
     }
 }
