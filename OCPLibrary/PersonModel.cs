d22340980acec4add248331443489e6a2dd4b18e
Mdsomratakbor <somrat876@gmail.com>
2020-06-19 23:31:16 +0600

    IApplicantModel Interface is create

--- OCPLibrary/PersonModel.cs
+++ OCPLibrary/PersonModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OCPLibrary
 {
-    public class PersonModel
+    public class PersonModel : IApplicantModel
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
